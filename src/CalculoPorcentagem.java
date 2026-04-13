461e17ad822a0d444c5606b344c61b000017f528
Darson <hiroshicontatopls@gmail.com>
2026-04-13 19:55:08 -0300

    Limpando repositório

--- src/CalculoPorcentagem.java
+++
@@ -1,8 +0,0 @@
-public class CalculoPorcentagem {
-    public static void main (String[]args) {
-        double salario = 1230.10d;
-        double porcentagem = 50d;
-        double calculo= salario * (porcentagem/100d);
-        System.out.println("50% do seu salário é équivalente a " + calculo);
-    }
-}
--- src/TestandoVariaveis.java
+++
@@ -1,6 +0,0 @@
-public class TestandoVariaveis {
-    public static void main(String[] agrs) {
-    char masculino='a';
-    char femiino='a';
-    }
-}
